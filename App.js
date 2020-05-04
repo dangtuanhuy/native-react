60a522a62b230eaccb2eae61d2c221736535cf00
Dang Tuan Huy <huytuand@hitachiconsulting.com>
2020-05-04 16:35:32 +0700

    save

---
+++ App.js
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
